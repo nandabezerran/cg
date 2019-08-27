8b38268994d9e2f3c144b7de16165ab2ee16c2ed
nandabezerran <fbn.prg@gmail.com>
2019-08-27 15:05:14 -0300

    Adiciona a criação de pontos do cone

--- biblioteca/Cubo.cpp
+++ biblioteca/Cubo.cpp
@@ -2,56 +2,46 @@
 // Created by thais on 26/08/2019.
 //
 
-/*
+
 
 #include "Cubo.hpp"
 #include "biblioteca.hpp"
 #include "Plano.hpp"
 
-Cubo::Cubo(VectorXd cNormal, double cAresta, Ponto* cCentro): normal(cNormal), aresta(cAresta), centro(cCentro), Objeto("Cubo"){
-    Ponto* p1 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y, this->centro->z);
-    Ponto* p2 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y, this->centro->z);
-    Ponto* p3 = biblioteca::CriarPonto(this->centro->x, this->centro->y + aresta/2, this->centro->z);
-    Ponto* p4 = biblioteca::CriarPonto(this->centro->x, this->centro->y - aresta/2, this->centro->z);
-    Ponto* p5 = biblioteca::CriarPonto(this->centro->x, this->centro->y, this->centro->z + aresta/2);
-    Ponto* p6 = biblioteca::CriarPonto(this->centro->x, this->centro->y, this->centro->z - aresta/2);
+Cubo::Cubo(double cAresta, Ponto* cCentro, VectorXd cNormal): normal(cNormal), aresta(cAresta), centro(cCentro), Objeto("Cubo"){
+    Ponto* p1 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y, this->centro->z + aresta/2);
+    Ponto* p2 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y, this->centro->z - aresta/2);
+    Ponto* p3 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y + aresta, this->centro->z - aresta/2);
+    Ponto* p4 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y + aresta, this->centro->z + aresta/2);
+    Ponto* p5 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y, this->centro->z + aresta/2);
+    Ponto* p6 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y, this->centro->z - aresta/2);
+    Ponto* p7 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y + aresta, this->centro->z - aresta/2);
+    Ponto* p8 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y + aresta, this->centro->z + aresta/2);
 
-    Plano* plano1 = new Plano(p1, this->normal);
-    Plano* plano2 = new Plano(p2, this->normal);
-    Plano* plano3 = new Plano(p3, this->normal);
-    Plano* plano4 = new Plano(p4, this->normal);
-    Plano* plano5 = new Plano(p5, this->normal);
-    Plano* plano6 = new Plano(p6, this->normal);
 
-    this->planos.push_back(plano1);
-    this->planos.push_back(plano2);
-    this->planos.push_back(plano3);
-    this->planos.push_back(plano4);
-    this->planos.push_back(plano5);
-    this->planos.push_back(plano6);
+
 }
 
-tuple<Ponto*,Ponto*> Cubo::IntersecaoReta(Ponto *pP0, VectorXd pVetor0, int pTamanho) {
-    vector<Ponto*> v_aux;
-    for(int i = 0; i<6; i++){
-        Ponto* p = this->planos[i]->IntersecaoRetaPlano(pP0, pVetor0, pTamanho);
-        if (p) {
-            v_aux.push_back(p);
-        }
-    }
+// tuple<Ponto*,Ponto*> Cubo::IntersecaoReta(Ponto *pP0, VectorXd pVetor0, int pTamanho) {
+    // vector<Ponto*> v_aux;
+    // for(int i = 0; i<6; i++){
+    //     Ponto* p = this->planos[i]->IntersecaoRetaPlano(pP0, pVetor0, pTamanho);
+    //     if (p) {
+    //         v_aux.push_back(p);
+    //     }
+    // }
 
-    if(v_aux.size() == 2){
-        return make_tuple(v_aux[0], v_aux[1]);
-    }
+    // if(v_aux.size() == 2){
+    //     return make_tuple(v_aux[0], v_aux[1]);
+    // }
 
-    if(v_aux.size() == 1){
-        return make_tuple(v_aux[0], nullptr);
-    }
+    // if(v_aux.size() == 1){
+    //     return make_tuple(v_aux[0], nullptr);
+    // }
 
-    if(v_aux.size() == 0){
-        return make_tuple(nullptr, nullptr);
-    }
-}
+    // if(v_aux.size() == 0){
+    //     return make_tuple(nullptr, nullptr);
+    // }
+//}
 
 
-*/
--- biblioteca/Cubo.hpp
+++ biblioteca/Cubo.hpp
@@ -1,7 +1,7 @@
 //
 // Created by thais on 26/08/2019.
 //
-/*
+
 
 #ifndef COMPUTACAOGRAFICA_CUBO_HPP
 #define COMPUTACAOGRAFICA_CUBO_HPP
@@ -9,16 +9,24 @@
 #include "Objeto.hpp"
 #include "Plano.hpp"
 
+struct Aresta{
+    Vector3d aresta;
+    tuple<Ponto, Ponto> pontos;
+};
+
+struct Face{
+    vector<Ponto> vertices;
+    vector<Aresta> arestas;
+
+};
+
 class Cubo : public Objeto {
 public:
-    string nome;
     VectorXd normal;
     double aresta;
     Ponto* centro;
-    vector<Plano*> planos;
-    Cubo(VectorXd cNormal, double cAresta, Ponto* cCentro);
-    tuple<Ponto*,Ponto*> IntersecaoReta(Ponto* pP0, VectorXd pVetor0, int pTamanho) override;
+    vector<Face*> faces;
+    Cubo(double cAresta, Ponto* cCentro, VectorXd cNormal);
+    //tuple<Ponto*,Ponto*> IntersecaoReta(Ponto* pP0, VectorXd pVetor0, int pTamanho) override;
 };
 #endif //COMPUTACAOGRAFICA_CUBO_HPP
-
-*/
