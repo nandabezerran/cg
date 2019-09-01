e1e1b50b78e8970e68a3dc3de58093850f0ddb2b
Fernanda Bezerra <fbn.prg@gmail.com>
2019-09-01 15:03:34 -0300

    Remove this não necessários

--- biblioteca/Cubo.cpp
+++ biblioteca/Cubo.cpp
@@ -8,64 +8,37 @@
 #include "Plano.hpp"
 
 Cubo::Cubo(double cAresta, Ponto* cCentro): aresta(cAresta), centro(cCentro), Objeto("Cubo", PINK, false){
-    Ponto* p1 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y + aresta, this->centro->z + aresta/2);
-    Ponto* p2 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y + aresta, this->centro->z - aresta/2);
-    Ponto* p3 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y + aresta, this->centro->z - aresta/2);
-    Ponto* p4 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y + aresta, this->centro->z + aresta/2);
-    Ponto* p5 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y, this->centro->z + aresta/2);
-    Ponto* p6 = biblioteca::CriarPonto(this->centro->x - aresta/2, this->centro->y, this->centro->z - aresta/2);
-    Ponto* p7 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y, this->centro->z - aresta/2);
-    Ponto* p8 = biblioteca::CriarPonto(this->centro->x + aresta/2, this->centro->y, this->centro->z + aresta/2);
+    Ponto* p1 = biblioteca::CriarPonto(centro->x - aresta/2, centro->y + aresta, centro->z + aresta/2);
+    Ponto* p2 = biblioteca::CriarPonto(centro->x - aresta/2, centro->y + aresta, centro->z - aresta/2);
+    Ponto* p3 = biblioteca::CriarPonto(centro->x + aresta/2, centro->y + aresta, centro->z - aresta/2);
+    Ponto* p4 = biblioteca::CriarPonto(centro->x + aresta/2, centro->y + aresta, centro->z + aresta/2);
+    Ponto* p5 = biblioteca::CriarPonto(centro->x - aresta/2, centro->y, centro->z + aresta/2);
+    Ponto* p6 = biblioteca::CriarPonto(centro->x - aresta/2, centro->y, centro->z - aresta/2);
+    Ponto* p7 = biblioteca::CriarPonto(centro->x + aresta/2, centro->y, centro->z - aresta/2);
+    Ponto* p8 = biblioteca::CriarPonto(centro->x + aresta/2, centro->y, centro->z + aresta/2);
 
-    vector<Vertice*> vertices;
-    Vertice* v1 = this->CriarVertice(p1, "V1");
-    Vertice* v2 = this->CriarVertice(p2, "V2");
-    Vertice* v3 = this->CriarVertice(p3, "V3");
-    Vertice* v4 = this->CriarVertice(p4, "V4");
-    Vertice* v5 = this->CriarVertice(p5, "V5");
-    Vertice* v6 = this->CriarVertice(p6, "V6");
-    Vertice* v7 = this->CriarVertice(p7, "V7");
-    Vertice* v8 = this->CriarVertice(p8, "V8");
+    vertices.push_back(CriarVertice(p1, "V1"));
+    vertices.push_back(CriarVertice(p2, "V2"));
+    vertices.push_back(CriarVertice(p3, "V3"));
+    vertices.push_back(CriarVertice(p4, "V4"));
+    vertices.push_back(CriarVertice(p5, "V5"));
+    vertices.push_back(CriarVertice(p6, "V6"));
+    vertices.push_back(CriarVertice(p7, "V7"));
+    vertices.push_back(CriarVertice(p8, "V8"));
 
-    vertices.push_back(v1);
-    vertices.push_back(v2);
-    vertices.push_back(v3);
-    vertices.push_back(v4);
-    vertices.push_back(v5);
-    vertices.push_back(v6);
-    vertices.push_back(v7);
-    vertices.push_back(v8);
-
-    this->vertices = vertices;
-
-    Face* f1 = this->CriarFace(vertices[0], vertices[3], vertices[2], "F1");
-    Face* f2 = this->CriarFace(vertices[2], vertices[1], vertices[0], "F2");
-    Face* f3 = this->CriarFace(vertices[6], vertices[5], vertices[2], "F3");
-    Face* f4 = this->CriarFace(vertices[5], vertices[1], vertices[2], "F4");
-    Face* f5 = this->CriarFace(vertices[7], vertices[6], vertices[3], "F5");
-    Face* f6 = this->CriarFace(vertices[6], vertices[2], vertices[3], "F6");
-    Face* f7 = this->CriarFace(vertices[4], vertices[7], vertices[3], "F7");
-    Face* f8 = this->CriarFace(vertices[3], vertices[0], vertices[4], "F8");
-    Face* f9 = this->CriarFace(vertices[6], vertices[7], vertices[4], "F9");
-    Face* f10 = this->CriarFace(vertices[4], vertices[5], vertices[6], "F10");
-    Face* f11 = this->CriarFace(vertices[0], vertices[4], vertices[5], "F11");
-    Face* f12 = this->CriarFace(vertices[5], vertices[1], vertices[0], "F12");
-
-    vector<Face*> faces;
-    faces.push_back(f1);
-    faces.push_back(f2);
-    faces.push_back(f3);
-    faces.push_back(f4);
-    faces.push_back(f5);
-    faces.push_back(f6);
-    faces.push_back(f7);
-    faces.push_back(f8);
-    faces.push_back(f9);
-    faces.push_back(f10);
-    faces.push_back(f11);
-    faces.push_back(f12);
-
-    this->faces = faces;
+    faces.push_back(CriarFace(vertices[0], vertices[3], vertices[2], "F1"));
+    faces.push_back(CriarFace(vertices[2], vertices[1], vertices[0], "F2"));
+    faces.push_back(CriarFace(vertices[6], vertices[5], vertices[2], "F3"));
+    faces.push_back(CriarFace(vertices[5], vertices[1], vertices[2], "F4"));
+    faces.push_back(CriarFace(vertices[7], vertices[6], vertices[3], "F5"));
+    faces.push_back(CriarFace(vertices[6], vertices[2], vertices[3], "F6"));
+    faces.push_back(CriarFace(vertices[4], vertices[7], vertices[3], "F7"));
+    faces.push_back(CriarFace(vertices[3], vertices[0], vertices[4], "F8"));
+    faces.push_back(CriarFace(vertices[6], vertices[7], vertices[4], "F9"));
+    faces.push_back(CriarFace(vertices[4], vertices[5], vertices[6], "F10"));
+    faces.push_back(CriarFace(vertices[0], vertices[4], vertices[5], "F11"));
+    faces.push_back(CriarFace(vertices[5], vertices[1], vertices[0], "F12"));
+    
 }
 
 tuple<Ponto*,Ponto*> Cubo::IntersecaoReta(Ponto *pP0, VectorXd pVetor0, int pTamanho) {
