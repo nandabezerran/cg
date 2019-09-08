33fb6ee6363333b37efd24159f1ebeea8ea7bb7a
Fernanda Bezerra <fbn.prg@gmail.com>
2019-09-08 18:50:08 -0300

    Cria luzSpot

--- biblioteca/LuzSpot.cpp
+++ biblioteca/LuzSpot.cpp
@@ -9,3 +9,23 @@ LuzSpot::LuzSpot(float r, float g, float b, double x, double y, double z): Luz(r
     posicao->y = y;
     posicao->z = z;
 }
+
+double LuzSpot::calcularFatorDifuso(PontoIntersecao *p, Objeto *o) {
+    VectorXd PlPint = biblioteca::SubtracaoPontos(p->p, posicao, 3);
+    VectorXd l = biblioteca::NormalizaVetor(PlPint, 3);
+    if(biblioteca::ProdutoEscalar(-l, direcao, 3) > angAbertura){
+        return 0 ;
+    }
+
+    return biblioteca::ProdutoEscalar(l, o->calcularNormal(p->p), 3);
+}
+
+VectorXd LuzSpot::calcularIntensidadeDifusa(PontoIntersecao *p, Objeto *o) {
+    VectorXd Id(3);
+//  Id[0] = intensidadeRgb[0] * o->material->K[0];
+//  Id[1] = intensidadeRgb[1] * o->material->K[1];
+//  Id[2] = intensidadeRgb[2] * o->material->K[2];
+
+    Id = Id * calcularFatorDifuso(p, o);
+    return Id;
+}
