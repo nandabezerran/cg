07880b61bbe8b736ea1caf05c710b8237227147a
natalia-rivers <nataliarios@alu.ufc.br>
2019-11-09 13:04:11 -0300

    cenario opengl

---
+++ cmake-build-debug/CMakeFiles/opengll.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengll.dir/main.cpp.obj"
+  "libopengll.dll.a"
+  "opengll.exe"
+  "opengll.exe.manifest"
+  "opengll.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
