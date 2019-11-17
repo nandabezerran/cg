38077a91e50122809a6910508060f64af2e7382d
natalia-rivers <nataliarios@alu.ufc.br>
2019-11-17 15:55:24 -0300

    adicionando objeto do blender

---
+++ cmake-build-debug/CMakeFiles/opengl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl.dir/main.cpp.o"
+  "libopengl.dll.a"
+  "opengl.exe"
+  "opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,176 @@
+
+#include "GL\glew.h"
+
+#include "GL\gl.h"
+#include "GL\glu.h"
+
+#include "GL\glut.h"
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "glm\glm.hpp"
+#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
+#include "tiny_obj_loader.h"
+using namespace std;
+
+std::string inputfile = "mesa.obj";
+tinyobj::attrib_t attrib;
+std::vector<tinyobj::shape_t> shapes;
+std::vector<tinyobj::material_t> materials;
+
+std::string warn;
+std::string err;
+
+void load(void)  {
+    bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, inputfile.c_str());
+
+    if (!warn.empty()) {
+        std::cout << warn << std::endl;
+    }
+
+    if (!err.empty()) {
+        std::cerr << err << std::endl;
+    }
+
+    if (!ret) {
+        exit(1);
+    }
+
+// Loop over shapes
+    for (size_t s = 0; s < shapes.size(); s++) {
+        // Loop over faces(polygon)
+        size_t index_offset = 0;
+        for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+            int fv = shapes[s].mesh.num_face_vertices[f];
+
+            // Loop over vertices in the face.
+            for (size_t v = 0; v < fv; v++) {
+                // access to vertex
+                tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+                tinyobj::real_t vx = attrib.vertices[3*idx.vertex_index+0];
+                tinyobj::real_t vy = attrib.vertices[3*idx.vertex_index+1];
+                tinyobj::real_t vz = attrib.vertices[3*idx.vertex_index+2];
+                tinyobj::real_t nx = attrib.normals[3*idx.normal_index+0];
+                tinyobj::real_t ny = attrib.normals[3*idx.normal_index+1];
+                tinyobj::real_t nz = attrib.normals[3*idx.normal_index+2];
+                tinyobj::real_t tx = attrib.texcoords[2*idx.texcoord_index+0];
+                tinyobj::real_t ty = attrib.texcoords[2*idx.texcoord_index+1];
+                // Optional: vertex colors
+                // tinyobj::real_t red = attrib.colors[3*idx.vertex_index+0];
+                // tinyobj::real_t green = attrib.colors[3*idx.vertex_index+1];
+                // tinyobj::real_t blue = attrib.colors[3*idx.vertex_index+2];
+            }
+            index_offset += fv;
+
+            // per-face material
+            shapes[s].mesh.material_ids[f];
+        }
+    }
+}
+
+
+
+void wall(double thickness)
+{
+    glPushMatrix();
+    glTranslated(0.5,0.5*thickness,0.5);
+    glScaled(1.0,thickness,1.0);
+    glutSolidCube(1.0);
+    glPopMatrix();
+}
+
+
+
+void displaySolid(void) {
+
+
+    GLfloat mat_ambient[]={0.7f,0.7f,0.7f,1.0f};
+    GLfloat mat_diffuse[]={0.5f,0.5f,0.5f,1.0f};
+    GLfloat mat_specular[]={1.0f,1.0f,1.0f,1.0f};
+    GLfloat mat_shininess[]={50.0f};
+
+    glMaterialfv(GL_FRONT,GL_AMBIENT,mat_ambient);
+    glMaterialfv(GL_FRONT,GL_DIFFUSE,mat_diffuse);
+    glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular);
+    glMaterialfv(GL_FRONT,GL_SHININESS,mat_shininess);
+    GLfloat lightIntensity[]={0.7f,0.7f,0.7f,0.7f};
+    GLfloat light_position[]={2.0f,6.0f,3.0f,0.0f};
+
+    //The glLightfv function returns light source parameter values.
+    glLightfv(GL_LIGHT0,GL_POSITION,light_position);
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,lightIntensity);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    double winHt=1.0;
+    glOrtho(-winHt*64/48.0,winHt*64/48.0,-winHt,winHt,0.1,100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(2.3,1.3,2.0,0.0,0.25,0.0,0.0,1.0,0.0);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+
+    glTranslatef ( 0.0f, -2.0f, 0.0f );
+
+    glPushMatrix();
+    glTranslated(0.4,0.4,0.6);
+    glRotated(45,0,0,1);
+    glScaled(0.08,0.08,0.08);
+    glPopMatrix();
+    glPushMatrix();
+    glTranslated(0.6,0.38,0.5);
+    glRotated(30,0,1,0);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslated(0.25,0.42,0.35);
+    glPopMatrix();
+    glPushMatrix();
+    glTranslated(0.4,0,0.4);
+    glPopMatrix();
+
+
+    wall(0.02);
+    glPushMatrix();
+    glRotated(90.0,0.0,0.0,1.0);    //draw second wall after rotating x axis by 90degre
+    wall(0.02);
+    glPopMatrix();
+    glPushMatrix();
+    glRotated(-90.0,1.0,0.0,0.0);   //draw floor
+    wall(0.02);
+    glPopMatrix();
+    glFlush();
+}
+static std::string GetBaseDir(const std::string& filepath) {
+    if (filepath.find_last_of("/\\") != std::string::npos)
+        return filepath.substr(0, filepath.find_last_of("/\\"));
+    return "";
+}
+
+int main(int argc,char **argv) {
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("Simple shaded scene consisting of a teapot");
+    glutDisplayFunc(displaySolid);
+    glutDisplayFunc(load);
+    glEnable(GL_LIGHTING);
+    std::vector< glm::vec3 > vertices;
+    std::vector< glm::vec2 > uvs;
+    std::vector< glm::vec3 > normals; // Won't be used at the moment.
+    std::string base_dir = GetBaseDir("mesa.obj");
+    bool res = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, "mesa.obj",
+                                base_dir.c_str());
+
+
+    glBufferData(GL_DRAW_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+    glEnable(GL_LIGHT0);
+    glShadeModel(GL_SMOOTH);//Specifies a symbolic value representing a shading technique. Accepted values are GL_FLAT and GL_SMOOTH.
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_NORMALIZE);
+    glClearColor(0.1,0.1,0.1,0.0);
+    glViewport(0,0,640,480);
+    glutMainLoop();
+    }
+
