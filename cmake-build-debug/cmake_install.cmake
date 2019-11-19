bb197c1c1dc1fd7fbc57fe0e346bbaeeb162112e
natalia-rivers <nataliarios@alu.ufc.br>
2019-11-19 10:57:54 -0300

    abrir mesa.obj

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/natal/CLionProjects/opengl
+# Install script for directory: C:/Users/natal/CLionProjects/ComputacaoGrafica
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/natal/CLionProjects/opengl/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/natal/CLionProjects/ComputacaoGrafica/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
