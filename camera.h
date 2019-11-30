877b5bf8d96d28d49e715c7f33a79466ddb2ebd9
natalia <nataliarios@alu.ufc.br>
2019-11-30 14:30:15 -0300

    opengl

---
+++ camera.h
@@ -0,0 +1,13 @@
+/* PARÂMETROS DOS MOVIMENTOS DA CÂMERA */
+
+float rot = 0.0f;
+float rot2 = 0.0f;
+
+float transZ = 0.0f;
+float transX = 0.0f;
+
+float angleAlpha = 0.0f;
+float angleBeta = 0.0f;
+float pi = 3.14159265359;
+
+/*---------------------------------------*/
---
+++ objetos/modelsGL.h
@@ -0,0 +1,8 @@
+#include "./paredes.h"
+#include "./piso.h"
+#include "./sofa.h"
+#include "./almofadas_sofa.h"
+#include "./poltrona.h"
+#include "./almofadas_poltrona.h"
+#include "./mesas.h"
+
