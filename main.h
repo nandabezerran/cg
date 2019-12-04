3ccd83b2934c8cf7aded5795f9c4609cb2cac926
natalia <nataliarios@alu.ufc.br>
2019-12-04 12:19:29 -0300

    voltando

---
+++ main.h
@@ -0,0 +1,25 @@
+// OBJETOS
+
+#include "./objetos/paredes.h"
+#include "./objetos/piso.h"
+#include "./objetos/sofa.h"
+#include "./objetos/almofadas_sofa.h"
+#include "./objetos/poltrona.h"
+#include "./objetos/almofadas_poltrona.h"
+#include "./objetos/mesas.h"
+#include "./objetos/abajur.h"
+#include "./objetos/abajur_suporte.h"
+#include "./objetos/armario.h"
+#include "./objetos/armario_gavetas.h"
+#include "./objetos/armario_puxadores.h"
+#include "./objetos/travesseiro1.h"
+#include "./objetos/travesseiro2.h"
+#include "./objetos/travesseiro3.h"
+#include "./objetos/travesseiro4.h"
+
+// FUNCOES
+
+#include "./materiais.h"
+#include "./camera.h"
+#include "./drawScene.h"
+#include "./menu.h"
--- objetos/modelsGL.h
+++
@@ -1,8 +0,0 @@
-#include "./paredes.h"
-#include "./piso.h"
-#include "./sofa.h"
-#include "./almofadas_sofa.h"
-#include "./poltrona.h"
-#include "./almofadas_poltrona.h"
-#include "./mesas.h"
-
